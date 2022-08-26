cade66ec994a2f910dbdf32ebf961450effff2d5
FlameyosFlow <49708496+coderFlameyosFlow@users.noreply.github.com>
2022-08-26 22:13:16 +0200

    Update company.py

--- cogs/economy/company.py
+++ cogs/economy/company.py
@@ -31,142 +31,140 @@ class CompanyCommand(commands.Cog):
 
     @company.subcommand(name="upgrade", description="Upgrade your company!")
     async def company_upgrade(self, interaction):
-        async with interaction.channel.typing():
-            member = interaction.user
-            findcompany = await companyaa.find_one({"_id": member.id})
-            if not findcompany:
-                await interaction.send("You currently don't have a company! You can create one by `/company create`.")
+        member = interaction.user
+        findcompany = await companyaa.find_one({"_id": member.id})
+        if not findcompany:
+            await interaction.send("You currently don't have a company! You can create one by `/company create`.")
 
-            findbank = await collection.find_one({"_id": member.id})
-            if not findbank:
-                await collection.insert_one({"_id": member.id, "wallet": 0})
+        findbank = await collection.find_one({"_id": member.id})
+        if not findbank:
+            await collection.insert_one({"_id": member.id, "wallet": 0})
 
-            wallet = findbank["wallet"]
+        wallet = findbank["wallet"]
 
-            level = findcompany["level"]
+        level = findcompany["level"]
 
-            while level < 3:
-                async def btncallback(interaction):
-                    if level == 1:
-                        if wallet >= 35000:
-                            await collection.update_one({
-                                    "_id": member.id
-                                },
+        while level < 3:
+            async def btncallback(interaction):
+                if level == 1:
+                    if wallet >= 35000:
+                        await collection.update_one({
+                                "_id": member.id
+                            },
 
-                                {
-                                    "$set": {
-                                        "wallet": wallet - 35000
-                                    }
-                                })
+                            {
+                                "$set": {
+                                    "wallet": wallet - 35000
+                                }
+                            })
 
-                            await companyaa.update_one({
-                                    "_id": member.id
-                                },
+                        await companyaa.update_one({
+                                "_id": member.id
+                            },
 
-                                {
-                                    "$set": {
-                                        "level": level + 1
-                                    }
-                                })
+                            {
+                                "$set": {
+                                    "level": level + 1
+                                }
+                            })
 
-                            await interaction.send("Your company level has been upgraded to level 2!", ephemeral=True)
-                            pass
+                        await interaction.send("Your company level has been upgraded to level 2!", ephemeral=True)
+                        pass
 
-                        else:
-                            await interaction.send("You don't even have enough money.", ephemeral=True)
-                            pass
-                    
-                    if level == 2:
-                        if wallet >= 100000:
-                            await collection.update_one({
-                                    "_id": member.id
-                                },
+                    else:
+                        await interaction.send("You don't even have enough money.", ephemeral=True)
+                        pass
 
-                                {
-                                    "$set": {
-                                        "wallet": wallet - 100000
-                                    }
-                                })
+                if level == 2:
+                    if wallet >= 100000:
+                        await collection.update_one({
+                                "_id": member.id
+                            },
 
-                            await companyaa.update_one({
-                                    "_id": member.id
-                                },
+                            {
+                                "$set": {
+                                    "wallet": wallet - 100000
+                                }
+                            })
 
-                                {
-                                    "$set": {
-                                        "level": level + 1
-                                    }
-                                })
+                        await companyaa.update_one({
+                                "_id": member.id
+                            },
+
+                            {
+                                "$set": {
+                                    "level": level + 1
+                                }
+                            })
 
 
-                            await interaction.send("Your company level has been upgraded to level 3!", ephemeral=True)
-                            pass
+                        await interaction.send("Your company level has been upgraded to level 3!", ephemeral=True)
+                        pass
 
-                        else:
-                            await interaction.send("You don't even have enough money.", ephemeral=True)
-                            pass
+                    else:
+                        await interaction.send("You don't even have enough money.", ephemeral=True)
+                        pass
 
-                async def btn2callback(interaction):
-                    await interaction.send("You have declined this.", ephemeral=True)
-                    pass
+            async def btn2callback(interaction):
+                await interaction.send("You have declined this.", ephemeral=True)
+                pass
 
-                button = Button(
-                    label="Yes",
-                    style=nextcord.ButtonStyle.green
-                )
+            button = Button(
+                label="Yes",
+                style=nextcord.ButtonStyle.green
+            )
 
-                button2 = Button(
-                    label="No",
-                    style=nextcord.ButtonStyle.red
-                )
+            button2 = Button(
+                label="No",
+                style=nextcord.ButtonStyle.red
+            )
 
-                button.callback = btncallback
-                button2.callback = btn2callback
-                view=View()
-                view.add_item(button)
-                view.add_item(button2) 
-                    
-                embed = nextcord.Embed(
-                    title=f"Would you like to upgrade your company to level {level + 1:,}?",
-                    description="It'll cost $35,000.",
-                    color=nextcord.Color.green(),
-                    timestamp=datetime.datetime.utcnow()
-                ).set_thumbnail(url=interaction.user.avatar.url)
-                await interaction.send(embed=embed, view=view)
-                return
+            button.callback = btncallback
+            button2.callback = btn2callback
+            view=View()
+            view.add_item(button)
+            view.add_item(button2) 
 
-            else:
-                await interaction.send("You hit the max level for your company, maybe stay tuned for more")
+            embed = nextcord.Embed(
+                title=f"Would you like to upgrade your company to level {level + 1:,}?",
+                description="It'll cost $35,000.",
+                color=nextcord.Color.green(),
+                timestamp=datetime.datetime.utcnow()
+            ).set_thumbnail(url=interaction.user.avatar.url)
+            await interaction.send(embed=embed, view=view)
+            return
+
+        else:
+            await interaction.send("You hit the max level for your company, maybe stay tuned for more")
 
     @company.subcommand(name="info", description="Information about your company!")
     async def company_info(self, interaction):
-        async with interaction.channel.typing():
-            member = interaction.user
-            findcompany = await companyaa.find_one({"_id": member.id})
-            if not findcompany:
-                await interaction.send("You don't have a company, create one with /company create.")
+        member = interaction.user
+        findcompany = await companyaa.find_one({"_id": member.id})
+        if not findcompany:
+            await interaction.send("You don't have a company, create one with /company create.")
 
-            worth = findcompany["worth"]
-            level = findcompany["level"]
-            name = findcompany["name"]
-            embed = nextcord.Embed(
-                title="{} information:".format(name),
-                description="A list of information about your company",
-                color=nextcord.Color.green(),
-                timestamp=datetime.datetime.utcnow()
-            )
+        worth = findcompany["worth"]
+        level = findcompany["level"]
+        name = findcompany["name"]
+        embed = nextcord.Embed(
+            title="{} information:".format(name),
+            description="A list of information about your company",
+            color=nextcord.Color.green(),
+            timestamp=datetime.datetime.utcnow()
+        )
 
-            embed.add_field(
-                name="Company Worth:",
-                value=f"{round(worth)}",
-            )
+        embed.add_field(
+            name="Company Worth:",
+            value=f"{round(worth)}",
+        )
 
-            embed.add_field(
-                name="Company Level:",
-                value=f"{int(level)}",
-            )
+        embed.add_field(
+            name="Company Level:",
+            value=f"{int(level)}",
+        )
 
-            await interaction.send(embed=embed)
+        await interaction.send(embed=embed)
 
     @company.subcommand(name="create", description="Create a company!")
     async def company_create(self, interaction):
